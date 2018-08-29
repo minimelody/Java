9e115b4cefa721bde5e57353784c139891798f80
minimelody <minimelodi@naver.com>
2018-08-30 02:31:24 +0900

    Scanner로 나이 입력

---
+++ day02/src/test/kh/java/JavaTestTwo.java
@@ -0,0 +1,15 @@
+package test.kh.java;
+
+import java.util.Scanner;
+
+public class JavaTestTwo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int age;
+		System.out.println("당신의 나이를 입력하세요:");
+		age = sc.nextInt();
+		System.out.println("당신은 " + age + "세 이군요");
+	}
+
+}
