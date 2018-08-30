b763a6e11963e3792a4ff9b5a736e78bf85170d8
minimelody <minimelodi@naver.com>
2018-08-30 19:05:07 +0900

    반복문 while을 이용하여 5개의 입력받은 정수 값의 합 구하기

--- day05/src/kh/java/test/testMain.java
+++ day05/src/kh/java/test/testMain.java
@@ -1,12 +1,51 @@
 package kh.java.test;
 
+import java.util.Scanner;
+
 public class testMain {
 
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		//c.cal();
 		HowManyDays days = new HowManyDays();
-		days.testDay();
+		//days.testDay();
+		
+		Scanner sc = new Scanner(System.in);
+		int num;
+		int sum = 0;
+		int i;
+		i=0;
+		
+		/* 반복문을 이용!
+		System.out.print("정수값을 입력하세요 : ");
+		num = sc.nextInt();
+		sum = sum + num;
+		
+		System.out.print("정수값을 입력하세요 : ");
+		num = sc.nextInt();
+		sum = sum + num;
+		
+		System.out.print("정수값을 입력하세요 : ");
+		num = sc.nextInt();
+		sum = sum + num;
+		
+		System.out.print("정수값을 입력하세요 : ");
+		num = sc.nextInt();
+		sum = sum + num;
+		
+		System.out.print("정수값을 입력하세요 : ");
+		num = sc.nextInt();
+		sum = sum + num;
+		*/
+		
+		while(i<5) {
+			i++;
+			System.out.print("정수값을 입력하세요 : ");
+			num = sc.nextInt();
+			sum = sum + num;
+		}
+		
+		System.out.println("입력한 5개의 정수의 합 : " + sum);
 
 	}
 
