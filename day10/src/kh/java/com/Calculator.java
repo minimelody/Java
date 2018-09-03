1359b1784cdf8456a4e29434f56603654d12d1b2
minimelody <minimelodi@naver.com>
2018-09-03 17:03:01 +0900

    메소드를 이용하여 기능별로 구분 - 덧셈, 뺄셈, 곱하기, 나누기

---
+++ day10/src/kh/java/com/Calculator.java
@@ -0,0 +1,24 @@
+package kh.java.com;
+
+public class Calculator {
+	public void start() {
+		int a = 10;
+		int b = 3;
+		System.out.println(addFunction(a,b));
+		System.out.println(subFunction(a,b));
+		System.out.println(mulFunction(a,b));
+		System.out.println(divFunction(a,b));
+	}
+	public int addFunction(int su1, int su2) {
+		return su1 + su2;
+	}
+	public int subFunction(int su1, int su2) {
+		return su1 - su2;
+	}
+	public int mulFunction(int su1, int su2) {
+		return su1 * su2;
+	}
+	public double divFunction(int su1, int su2) {
+		return su1 / (double)su2;
+	}
+}
---
+++ day10/src/kh/java/com/TestMain.java
@@ -0,0 +1,11 @@
+package kh.java.com;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		cal.start();
+
+	}
+
+}
